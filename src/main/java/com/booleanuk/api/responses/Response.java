b0b4fec2ca5dfd8abead9125da8e5c6692950ef4
shyye <emma.art.dev@gmail.com>
2024-09-17 08:12:05 +0200

    Add Response and ErrorResponse

---
+++ src/main/java/com/booleanuk/api/responses/Response.java
@@ -0,0 +1,15 @@
+package com.booleanuk.api.responses;
+
+
+import lombok.Getter;
+
+@Getter
+public class Response<T> {
+    protected String status;
+    protected T data;
+
+    public void set(T data) {
+        this.status = "success";
+        this.data = data;
+    }
+}
