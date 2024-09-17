7466009882591a312efd8e1bfccb1d44ded0bec6
shyye <emma.art.dev@gmail.com>
2024-09-17 08:14:57 +0200

    Add Security configuration and login

---
+++ src/main/java/com/booleanuk/api/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.booleanuk.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("login")
+    String login() {
+        return "login";
+    }
+}
