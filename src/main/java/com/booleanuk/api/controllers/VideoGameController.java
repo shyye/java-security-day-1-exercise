b062750e0ebd4a0625f62bd8e3768e8cb951809c
shyye <emma.art.dev@gmail.com>
2024-09-17 14:49:26 +0200

    Update VideoGameController with create, update and delete

--- src/main/java/com/booleanuk/api/controllers/VideoGameController.java
+++ src/main/java/com/booleanuk/api/controllers/VideoGameController.java
@@ -9,16 +9,21 @@ import com.booleanuk.api.responses.VideoGameResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("games")
 public class VideoGameController {
     @Autowired
     private VideoGameRepository repository;
+
+    @PostMapping
+    public ResponseEntity<VideoGameResponse> create(@RequestBody VideoGame game) {
+        this.repository.save(game);
+        VideoGameResponse response = new VideoGameResponse();
+        response.set(game);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 
     @GetMapping
     public ResponseEntity<VideoGameListResponse> getAll() {
@@ -35,6 +40,41 @@ public class VideoGameController {
             error.set("A game with this id was not found.");
             return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
         }
+        VideoGameResponse response = new VideoGameResponse();
+        response.set(game);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Response<?>> update(@PathVariable int id, @RequestBody VideoGame game) {
+        VideoGame originalGame = this.repository.findById(id).orElse(null);
+        if (originalGame == null) {
+            ErrorResponse error = new ErrorResponse();
+            error.set("Not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+        game.setId(id);
+        try {
+            this.repository.save(game);
+        } catch (Exception e) {
+            ErrorResponse error = new ErrorResponse();
+            error.set("Bad request");
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
+        VideoGameResponse response = new VideoGameResponse();
+        response.set(game);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Response<?>> delete(@PathVariable int id) {
+        VideoGame game = this.repository.findById(id).orElse(null);
+        if (game == null) {
+            ErrorResponse error = new ErrorResponse();
+            error.set("Not found");
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+        this.repository.delete(game);
         VideoGameResponse response = new VideoGameResponse();
         response.set(game);
         return ResponseEntity.ok(response);
